8634b3d181a7b177a04d3fcb90d6ff3bbba7c2f1
Danila <novash.danila@yandex.by>
2025-11-19 22:23:31 +0300

    fix: исправлена работа с конфигом

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as path from 'node:path';
 import { configProvider } from './app.config.provider';
 import { FilmsController } from './films/films.controller';
@@ -20,7 +20,12 @@ import {
       isGlobal: true,
       cache: true,
     }),
-    MongooseModule.forRoot(process.env.DATABASE_URL),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'),
+      }),
+    }),
     MongooseModule.forFeature([{ name: FilmModelName, schema: FilmSchema }]),
     ServeStaticModule.forRoot({
       rootPath: path.join(__dirname, '..', 'public', 'content', 'afisha'),
