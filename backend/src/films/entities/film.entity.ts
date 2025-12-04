1c4392c1e0985400cf9b914a814b91439ad991af
Danila <novash.danila@yandex.by>
2025-12-04 22:36:08 +0300

    feat: добавлены сущности фильма и расписания

---
+++ backend/src/films/entities/film.entity.ts
@@ -0,0 +1,35 @@
+import { Column, Entity, OneToMany, PrimaryColumn, } from "typeorm";
+import { ScheduleEntity } from "./schedule.entity";
+
+@Entity('films')
+export class FilmEntity {
+  @PrimaryColumn()
+  id: string
+
+  @Column()
+  rating: number
+
+  @Column()
+  director: string
+
+  @Column()
+  tags: string[]
+
+  @Column()
+  image: string
+
+  @Column()
+  cover: string
+
+  @Column()
+  title: string
+
+  @Column()
+  about: string
+
+  @Column()
+  description: string
+
+  @OneToMany(() => ScheduleEntity, (schedule) => schedule.film)
+  schedule: ScheduleEntity[]
+}
